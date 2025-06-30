52649f37e70b59be8b7e51d4973452d0103209aa
dashygo097 <dashygo097@gmail.com>
2025-06-30 18:20:47 +0800

    bug: device class does not work; checkpoint

---
+++ src/interface/device.cc
@@ -0,0 +1,121 @@
+#include "interface/device.hpp"
+
+#include "interface/instance.hpp"
+#include <set>
+#include <vector>
+
+Device::Device(VkInstance instance, VkSurfaceKHR surface,
+               std::vector<const char *> deviceExtensions,
+               std::vector<const char *> validationLayers)
+    : instance(instance), surface(surface) {
+  uint32_t deviceCount = 0;
+  vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+  if (deviceCount == 0) {
+    throw std::runtime_error("failed to find GPUs with Vulkan support!");
+  }
+
+  std::vector<VkPhysicalDevice> devices(deviceCount);
+  vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+  for (const auto &device : devices) {
+    if (isSuitable(device)) {
+      physicalDevice = device;
+      break;
+    }
+  }
+
+  if (physicalDevice == VK_NULL_HANDLE) {
+    throw std::runtime_error("failed to find a suitable GPU!");
+  }
+
+  QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+  std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+  std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(),
+                                            indices.presentFamily.value()};
+
+  float queuePriority = 1.0f;
+  for (uint32_t queueFamily : uniqueQueueFamilies) {
+    VkDeviceQueueCreateInfo queueCreateInfo{};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = queueFamily;
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+    queueCreateInfos.push_back(queueCreateInfo);
+  }
+
+  VkPhysicalDeviceFeatures deviceFeatures{};
+
+  VkDeviceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+  createInfo.queueCreateInfoCount =
+      static_cast<uint32_t>(queueCreateInfos.size());
+  createInfo.pQueueCreateInfos = queueCreateInfos.data();
+
+  createInfo.pEnabledFeatures = &deviceFeatures;
+
+  createInfo.enabledExtensionCount =
+      static_cast<uint32_t>(deviceExtensions.size());
+  createInfo.ppEnabledExtensionNames = deviceExtensions.data();
+
+  if (ENABLE_VALIDATION_LAYERS) {
+    createInfo.enabledLayerCount =
+        static_cast<uint32_t>(validationLayers.size());
+    createInfo.ppEnabledLayerNames = validationLayers.data();
+  } else {
+    createInfo.enabledLayerCount = 0;
+  }
+
+  if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &logicalDevice) !=
+      VK_SUCCESS) {
+    throw std::runtime_error("failed to create logical device!");
+  }
+
+  vkGetDeviceQueue(logicalDevice, indices.graphicsFamily.value(), 0,
+                   &graphicsQueue);
+  vkGetDeviceQueue(logicalDevice, indices.presentFamily.value(), 0,
+                   &presentQueue);
+}
+
+Device::~Device() {
+  if (logicalDevice != VK_NULL_HANDLE) {
+    vkDestroyDevice(logicalDevice, nullptr);
+  }
+}
+
+VkDevice Device::getVkDevice() const { return logicalDevice; }
+VkPhysicalDevice Device::getVkPhysicalDevice() const { return physicalDevice; }
+VkQueue Device::getGraphicsQueue() const { return graphicsQueue; }
+VkQueue Device::getPresentQueue() const { return presentQueue; }
+
+bool Device::isSuitable(VkPhysicalDevice pDevice) {
+  QueueFamilyIndices indices = findQueueFamilies(pDevice);
+  return indices.isComplete();
+}
+
+QueueFamilyIndices Device::findQueueFamilies(VkPhysicalDevice pDevice) {
+  QueueFamilyIndices indices;
+  uint32_t queueFamilyCount = 0;
+  vkGetPhysicalDeviceQueueFamilyProperties(pDevice, &queueFamilyCount, nullptr);
+  std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+  vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount,
+                                           queueFamilies.data());
+  int i = 0;
+  for (const auto &queueFamily : queueFamilies) {
+    if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+      indices.graphicsFamily = i;
+    }
+    VkBool32 presentSupport = false;
+    vkGetPhysicalDeviceSurfaceSupportKHR(pDevice, i, surface, &presentSupport);
+    if (presentSupport) {
+      indices.presentFamily = i;
+    }
+    if (indices.isComplete()) {
+      break;
+    }
+    i++;
+  }
+  return indices;
+}
