b669ea49d164c1463551629d6a3ae641c23e6f8a
dashygo097 <dashygo097@github.com>
2025-07-01 14:09:18 +0800

    checkpoint: swapchain

--- include/interface/swapchain.hpp
+++ include/interface/swapchain.hpp
@@ -18,6 +18,7 @@ VkPresentModeKHR chooseSwapPresentMode(
 VkExtent2D chooseSwapExtent(GLFWwindow *window,
                             const VkSurfaceCapabilitiesKHR &capabilities);
 SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+
 class SwapChain {
 public:
   SwapChain(GLFWwindow *window, VkDevice device,
@@ -25,6 +26,11 @@ public:
   ~SwapChain();
 
   VkSwapchainKHR getSwapChain() const { return swapChain; }
+  VkImage getImage(uint32_t index) const { return images.at(index); }
+  VkImageView getImageView(uint32_t index) const {
+    return imageViews.at(index);
+  }
+  VkFormat getFormat() const { return format; }
   VkExtent2D getExtent() const { return extent; }
 
 private:
