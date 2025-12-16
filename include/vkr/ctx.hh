80bf5686c096cfa02f9d77e08ca7c51ef60a2517
dashygo097 <dashygo097@github.com>
2025-12-16 18:49:47 +0800

    checkpoint

--- include/vkr/ctx.hh
+++ include/vkr/ctx.hh
@@ -9,79 +9,78 @@
 namespace vkr {
 
 struct VulkanContext {
-
   // --- App/Engine Info ---
-  std::string appName;
-  std::string engineName;
-  uint32_t appVersion;
-  uint32_t engineVersion;
+  std::string appName{};
+  std::string engineName{};
+  uint32_t appVersion{};
+  uint32_t engineVersion{};
 
   // --- Window Info ---
-  uint32_t width;
-  uint32_t height;
-  std::string title;
-
-  // --- Camera Info ---
-  bool cameraEnabled;
-  bool cameraLocked;
-  float cameraMovementSpeed;
-  float cameraMouseSensitivity;
-  float cameraFov;
-  float cameraAspectRatio;
-  float cameraNearPlane;
-  float cameraFarPlane;
+  uint32_t width{};
+  uint32_t height{};
+  std::string title{"Vulkan Application"};
 
   // --- Shader paths ---
-  std::string vertexShaderPath;
-  std::string fragmentShaderPath;
+  std::string vertexShaderPath{};
+  std::string fragmentShaderPath{};
 
   // --- Vulkan Core Objects ---
-  GLFWwindow *window = nullptr;
-  VkInstance instance = VK_NULL_HANDLE;
-  VkSurfaceKHR surface = VK_NULL_HANDLE;
-  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-  VkDevice device = VK_NULL_HANDLE;
+  GLFWwindow *window{nullptr};
+  VkInstance instance{VK_NULL_HANDLE};
+  VkSurfaceKHR surface{VK_NULL_HANDLE};
+  VkPhysicalDevice physicalDevice{VK_NULL_HANDLE};
+  VkDevice device{VK_NULL_HANDLE};
 
-  VkQueue graphicsQueue = VK_NULL_HANDLE;
-  VkQueue presentQueue = VK_NULL_HANDLE;
+  VkQueue graphicsQueue{VK_NULL_HANDLE};
+  VkQueue presentQueue{VK_NULL_HANDLE};
 
   // --- Swapchain ---
-  VkSwapchainKHR swapchain = VK_NULL_HANDLE;
-  std::vector<VkImage> swapchainImages = {};
-  std::vector<VkImageView> swapchainImageViews = {};
-  VkFormat swapchainImageFormat = VK_FORMAT_UNDEFINED;
-  VkExtent2D swapchainExtent = {};
-  std::vector<VkFramebuffer> swapchainFramebuffers = {};
+  VkSwapchainKHR swapchain{VK_NULL_HANDLE};
+  std::vector<VkImage> swapchainImages{};
+  std::vector<VkImageView> swapchainImageViews{};
+  VkFormat swapchainImageFormat{VK_FORMAT_UNDEFINED};
+  VkExtent2D swapchainExtent{};
+  std::vector<VkFramebuffer> swapchainFramebuffers{};
 
   // --- Rendering ---
-  VkRenderPass renderPass = VK_NULL_HANDLE;
-  VkDescriptorSetLayout descriptorSetLayout = VK_NULL_HANDLE;
-  VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
-  VkPipeline graphicsPipeline = VK_NULL_HANDLE;
+  VkRenderPass renderPass{VK_NULL_HANDLE};
+  VkDescriptorSetLayout descriptorSetLayout{VK_NULL_HANDLE};
+  VkPipelineLayout pipelineLayout{VK_NULL_HANDLE};
+  VkPipeline graphicsPipeline{VK_NULL_HANDLE};
 
   // --- CommandPool ---
-  VkCommandPool commandPool = VK_NULL_HANDLE;
+  VkCommandPool commandPool{VK_NULL_HANDLE};
 
   // --- Uniform Buffers ---
-  std::vector<VkBuffer> uniformBuffers = {};
-  std::vector<VkDeviceMemory> uniformBuffersMemory = {};
-  std::vector<void *> uniformBuffersMapped = {};
+  std::vector<VkBuffer> uniformBuffers{};
+  std::vector<VkDeviceMemory> uniformBuffersMemory{};
+  std::vector<void *> uniformBuffersMapped{};
 
   // --- Descriptor Sets ---
-  VkDescriptorPool descriptorPool = VK_NULL_HANDLE;
-  std::vector<VkDescriptorSet> descriptorSets = {};
+  VkDescriptorPool descriptorPool{VK_NULL_HANDLE};
+  std::vector<VkDescriptorSet> descriptorSets{};
 
   // --- Command Buffers ---
-  std::vector<VkCommandBuffer> commandBuffers = {};
+  std::vector<VkCommandBuffer> commandBuffers{};
 
   // --- Synchronization ---
-  std::vector<VkSemaphore> imageAvailableSemaphores = {};
-  std::vector<VkSemaphore> renderFinishedSemaphores = {};
-  std::vector<VkFence> inFlightFences = {};
+  std::vector<VkSemaphore> imageAvailableSemaphores{};
+  std::vector<VkSemaphore> renderFinishedSemaphores{};
+  std::vector<VkFence> inFlightFences{};
+
+  // --- Camera Info ---
+  bool cameraEnabled{};
+  bool cameraLocked{};
+  float cameraMovementSpeed{};
+  float cameraMouseSensitivity{};
+  float cameraFov{};
+  float cameraAspectRatio{};
+  float cameraNearPlane{};
+  float cameraFarPlane{};
 
   // --- Runtime ---
-  uint32_t currentFrame = 0;
-  bool framebufferResized = false;
+  uint32_t currentFrame{};
+  bool framebufferResized{};
 
   // --- Validation & Extensions ---
   std::vector<const char *> validationLayers = {"VK_LAYER_KHRONOS_validation"};
