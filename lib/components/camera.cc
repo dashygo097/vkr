56e48b81e371a66ca5198a15e9c9f1eadccd0918
dashygo097 <dashygo097@github.com>
2025-12-17 13:51:53 +0800

    feat: cameta out of ctx

--- lib/components/camera.cc
+++ lib/components/camera.cc
@@ -1,21 +1,19 @@
 #include "vkr/components/camera.hh"
 
 namespace vkr {
-Camera::Camera(GLFWwindow *window) : window(window) {}
-Camera::Camera(GLFWwindow *window, float movementSpeed, float mouseSensitivity,
-               float aspectRatio, float fov, float nearPlane, float farPlane)
-    : window(window), _movementSpeed(movementSpeed),
-      _mouseSensitivity(mouseSensitivity), _fov(fov), _aspectRatio(aspectRatio),
-      _nearPlane(nearPlane), _farPlane(farPlane) {}
-
-Camera::Camera(const VulkanContext &ctx)
-    : Camera(ctx.window, ctx.cameraMovementSpeed, ctx.cameraMouseSensitivity,
-             ctx.cameraAspectRatio, ctx.cameraFov, ctx.cameraNearPlane,
-             ctx.cameraFarPlane) {
-  _locked = ctx.cameraLocked;
+Camera::Camera(const Window &window, const float &movementSpeed,
+               const float &mouseSensitivity, const float &fov,
+               const float &aspectRatio, const float &nearPlane,
+               const float &farPlane)
+    : window(window), movementSpeed(movementSpeed),
+      mouseSensitivity(mouseSensitivity), fov(fov), aspectRatio(aspectRatio),
+      nearPlane(nearPlane), farPlane(farPlane), _pos(0.0f, 0.0f, 3.0f),
+      _front(0.0f, 0.0f, -1.0f), _worldUp(0.0f, 1.0f, 0.0f), _yaw(-90.0f),
+      _pitch(0.0f), _locked(false), lastX(window.width() / 2.0),
+      lastY(window.height() / 2.0), firstMouse(true) {
+  _right = glm::normalize(glm::cross(_front, _worldUp));
+  _up = glm::normalize(glm::cross(_right, _front));
 }
-
-Camera::~Camera() {}
 
 void Camera::track() {
   if (_locked)
@@ -31,27 +29,27 @@ void Camera::track() {
       1000.0f;
   lastTimeStamp = now;
 
-  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+  if (glfwGetKey(window.glfwWindow(), GLFW_KEY_W) == GLFW_PRESS) {
     moveForward(deltaTime);
   }
-  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+  if (glfwGetKey(window.glfwWindow(), GLFW_KEY_S) == GLFW_PRESS) {
     moveBackward(deltaTime);
   }
-  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+  if (glfwGetKey(window.glfwWindow(), GLFW_KEY_A) == GLFW_PRESS) {
     moveLeft(deltaTime);
   }
-  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+  if (glfwGetKey(window.glfwWindow(), GLFW_KEY_D) == GLFW_PRESS) {
     moveRight(deltaTime);
   }
-  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+  if (glfwGetKey(window.glfwWindow(), GLFW_KEY_SPACE) == GLFW_PRESS) {
     moveUp(deltaTime);
   }
-  if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+  if (glfwGetKey(window.glfwWindow(), GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
     moveDown(deltaTime);
   }
 
   double xpos, ypos;
-  glfwGetCursorPos(window, &xpos, &ypos);
+  glfwGetCursorPos(window.glfwWindow(), &xpos, &ypos);
   if (firstMouse) {
     lastX = xpos;
     lastY = ypos;
@@ -64,7 +62,8 @@ void Camera::track() {
   lastX = xpos;
   lastY = ypos;
 
-  if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+  if (glfwGetMouseButton(window.glfwWindow(), GLFW_MOUSE_BUTTON_LEFT) ==
+      GLFW_PRESS) {
     mouseMove(xoffset, yoffset);
   }
 }
