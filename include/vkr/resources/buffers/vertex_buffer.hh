51fbe2f170a4adf7a1d5be385b0ac837835a6a23
dashygo097 <dashygo097@gmail.com>
2025-12-19 11:12:44 +0800

    checkpoint: different types of vertex

--- include/vkr/resources/buffers/vertex_buffer.hh
+++ include/vkr/resources/buffers/vertex_buffer.hh
@@ -197,6 +197,121 @@ public:
   }
 };
 
+struct VertexNormal3D {
+  glm::vec3 pos;
+  glm::vec3 color;
+  glm::vec3 normal;
+
+  bool operator==(const VertexNormal3D &other) const {
+    return pos == other.pos && color == other.color && normal == other.normal;
+  }
+
+  static VkVertexInputBindingDescription getBindingDescription() {
+    VkVertexInputBindingDescription bindingDescription{};
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(VertexNormal3D);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    return bindingDescription;
+  }
+
+  static std::array<VkVertexInputAttributeDescription, 3>
+  getAttributeDescriptions() {
+    std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(VertexNormal3D, pos);
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(VertexNormal3D, color);
+    attributeDescriptions[2].binding = 0;
+    attributeDescriptions[2].location = 2;
+    attributeDescriptions[2].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[2].offset = offsetof(VertexNormal3D, normal);
+    return attributeDescriptions;
+  }
+};
+
+struct VertexTextured3D {
+  glm::vec3 pos;
+  glm::vec3 color;
+  glm::vec2 texCoord;
+
+  bool operator==(const VertexTextured3D &other) const {
+    return pos == other.pos && color == other.color &&
+           texCoord == other.texCoord;
+  }
+
+  static VkVertexInputBindingDescription getBindingDescription() {
+    VkVertexInputBindingDescription bindingDescription{};
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(VertexTextured3D);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    return bindingDescription;
+  }
+
+  static std::array<VkVertexInputAttributeDescription, 3>
+  getAttributeDescriptions() {
+    std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(VertexTextured3D, pos);
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(VertexTextured3D, color);
+    attributeDescriptions[2].binding = 0;
+    attributeDescriptions[2].location = 2;
+    attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[2].offset = offsetof(VertexTextured3D, texCoord);
+    return attributeDescriptions;
+  }
+};
+
+struct VertexNormalTexture3D {
+  glm::vec3 pos;
+  glm::vec3 color;
+  glm::vec3 normal;
+  glm::vec2 texCoord;
+
+  bool operator==(const VertexNormalTexture3D &other) const {
+    return pos == other.pos && color == other.color &&
+           texCoord == other.texCoord && normal == other.normal;
+  }
+
+  static VkVertexInputBindingDescription getBindingDescription() {
+    VkVertexInputBindingDescription bindingDescription{};
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(VertexNormalTexture3D);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    return bindingDescription;
+  }
+
+  static std::array<VkVertexInputAttributeDescription, 4>
+  getAttributeDescriptions() {
+    std::array<VkVertexInputAttributeDescription, 4> attributeDescriptions{};
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(VertexNormalTexture3D, pos);
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(VertexNormalTexture3D, color);
+    attributeDescriptions[2].binding = 0;
+    attributeDescriptions[2].location = 2;
+    attributeDescriptions[2].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[2].offset = offsetof(VertexNormalTexture3D, normal);
+    attributeDescriptions[3].binding = 0;
+    attributeDescriptions[3].location = 3;
+    attributeDescriptions[3].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[3].offset = offsetof(VertexNormalTexture3D, texCoord);
+    return attributeDescriptions;
+  }
+};
+
 } // namespace vkr
 
 namespace std {
@@ -223,4 +338,34 @@ template <> struct hash<vkr::Vertex3D> {
             1);
   }
 };
+
+template <> struct hash<vkr::VertexNormal3D> {
+  size_t operator()(vkr::VertexNormal3D const &vertex) const {
+    return (((hash<glm::vec3>()(vertex.pos) ^
+              (hash<glm::vec3>()(vertex.color) << 1)) >>
+             1) ^
+            (hash<glm::vec3>()(vertex.normal) << 1));
+  }
+};
+
+template <> struct hash<vkr::VertexTextured3D> {
+  size_t operator()(vkr::VertexTextured3D const &vertex) const {
+    return (((hash<glm::vec3>()(vertex.pos) ^
+              (hash<glm::vec3>()(vertex.color) << 1)) >>
+             1) ^
+            (hash<glm::vec2>()(vertex.texCoord) << 1));
+  }
+};
+
+template <> struct hash<vkr::VertexNormalTexture3D> {
+  size_t operator()(vkr::VertexNormalTexture3D const &vertex) const {
+    return ((((hash<glm::vec3>()(vertex.pos) ^
+               (hash<glm::vec3>()(vertex.color) << 1)) >>
+              1) ^
+             (hash<glm::vec3>()(vertex.normal) << 1)) >>
+            1) ^
+           (hash<glm::vec2>()(vertex.texCoord) << 1);
+  }
+};
+
 } // namespace std
