5908dd19e714ec1b0c7920f8f2772dc935e768dc
dashygo097 <dashygo097@github.com>
2026-02-22 21:58:49 +0800

    [tools.canvas] canvas

--- lib/scene/camera.cc
+++ lib/scene/camera.cc
@@ -15,20 +15,8 @@ Camera::Camera(const core::Window &window, const Timer &timer,
   right_ = glm::normalize(glm::cross(front_, world_up_));
   up_ = glm::normalize(glm::cross(right_, front_));
 
-  switch (mode_) {
-  case pipeline::PipelineMode::Default3D:
-  case pipeline::PipelineMode::Textured3D: {
-    glfwSetWindowUserPointer(window_.glfwWindow(), this);
-    glfwSetScrollCallback(window_.glfwWindow(), scrollCallback);
-    break;
-  }
-  case pipeline::PipelineMode::Default2D:
-  case pipeline::PipelineMode::Textured2D:
-  case pipeline::PipelineMode::NoVertices:
-  default: {
-    break;
-  }
-  }
+  glfwSetWindowUserPointer(window_.glfwWindow(), this);
+  glfwSetScrollCallback(window_.glfwWindow(), scrollCallback);
 }
 
 void Camera::track() {
@@ -37,58 +25,42 @@ void Camera::track() {
 
   auto deltaTime = timer_.deltaTime();
 
-  switch (mode_) {
-  case pipeline::PipelineMode::Default3D:
-  case pipeline::PipelineMode::Textured3D: {
-    if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_W) == GLFW_PRESS) {
-      moveForward(deltaTime);
-    }
-    if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_S) == GLFW_PRESS) {
-      moveBackward(deltaTime);
-    }
-    if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_A) == GLFW_PRESS) {
-      moveLeft(deltaTime);
-    }
-    if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_D) == GLFW_PRESS) {
-      moveRight(deltaTime);
-    }
-    if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_SPACE) == GLFW_PRESS) {
-      moveUp(deltaTime);
-    }
-    if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-      moveDown(deltaTime);
-    }
+  if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_W) == GLFW_PRESS) {
+    moveForward(deltaTime);
+  }
+  if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_S) == GLFW_PRESS) {
+    moveBackward(deltaTime);
+  }
+  if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_A) == GLFW_PRESS) {
+    moveLeft(deltaTime);
+  }
+  if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_D) == GLFW_PRESS) {
+    moveRight(deltaTime);
+  }
+  if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_SPACE) == GLFW_PRESS) {
+    moveUp(deltaTime);
+  }
+  if (glfwGetKey(window_.glfwWindow(), GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+    moveDown(deltaTime);
+  }
 
-    double xpos, ypos;
-    glfwGetCursorPos(window_.glfwWindow(), &xpos, &ypos);
-    if (first_mouse_) {
-      last_x_ = xpos;
-      last_y_ = ypos;
-      first_mouse_ = false;
-      return;
-    }
-    float xoffset = static_cast<float>(xpos - last_x_);
-    float yoffset = static_cast<float>(last_y_ - ypos);
-
+  double xpos, ypos;
+  glfwGetCursorPos(window_.glfwWindow(), &xpos, &ypos);
+  if (first_mouse_) {
     last_x_ = xpos;
     last_y_ = ypos;
-
-    if (glfwGetMouseButton(window_.glfwWindow(), GLFW_MOUSE_BUTTON_LEFT) ==
-        GLFW_PRESS) {
-      mouseMove(xoffset, yoffset);
-    }
-
-    // mouse scroll to control fov
-
-    break;
+    first_mouse_ = false;
+    return;
   }
+  float xoffset = static_cast<float>(xpos - last_x_);
+  float yoffset = static_cast<float>(last_y_ - ypos);
 
-  case pipeline::PipelineMode::Default2D:
-  case pipeline::PipelineMode::Textured2D:
-  case pipeline::PipelineMode::NoVertices:
-  default: {
-    break;
-  }
+  last_x_ = xpos;
+  last_y_ = ypos;
+
+  if (glfwGetMouseButton(window_.glfwWindow(), GLFW_MOUSE_BUTTON_LEFT) ==
+      GLFW_PRESS) {
+    mouseMove(xoffset, yoffset);
   }
 }
 
